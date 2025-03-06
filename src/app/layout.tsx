6f493b77d0349d0e835068a5b6a32f27a1ed325e
MuhammadJawaar <muhammadjawaar18@gmail.com>
2025-03-06 14:01:49 +0700

    delete some file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
+
 import ToastThemeProvider from "@/components/ToastThemeProvider/ToastThemeProvider";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
