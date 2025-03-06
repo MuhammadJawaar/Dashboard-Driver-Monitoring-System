f858b17a8b21e9aa2429b749323fa2cd7a6ee0ee
MuhammadJawaar <muhammadjawaar18@gmail.com>
2025-03-06 14:21:52 +0700

    error update

--- src/lib/authentication.ts
+++ src/lib/authentication.ts
@@ -1,23 +1,28 @@
 'use server';
- 
+
 import { signIn } from '../../auth';
 import { AuthError } from 'next-auth';
- 
+
 export async function authenticate(
   prevState: string | undefined,
   formData: FormData,
 ) {
   try {
     await signIn('credentials', formData);
-  } catch (error) {
+  } catch (error: unknown) {
     if (error instanceof AuthError) {
       switch (error.type) {
         case 'CredentialsSignin':
           return 'Invalid credentials.';
         default:
-          return 'Something went wrong.';
+          return `Authentication error: ${error.type}`;
       }
     }
-    throw error;
+
+    if (error instanceof Error) {
+      return `Unexpected error: ${error.message}`;
+    }
+
+    return 'An unknown error occurred.';
   }
 }
